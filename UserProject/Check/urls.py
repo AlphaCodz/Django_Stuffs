5fe291184fd085a0b18d5e7da9f0b0c6a16fca35
AlphaCodz <adeyanjuabiona69@gmail.com>
2022-07-07 11:54:04 +0100

    Django CBV

--- UserProject/Check/urls.py
+++ UserProject/Check/urls.py
@@ -1,9 +1,10 @@
 from Check import views
 from django.shortcuts import render
 from django.urls import path, re_path
-from .views import TaskDetail, TaskList
+from .views import TaskCreate, TaskDetail, TaskList
 
 urlpatterns = [
     path('', TaskList.as_view(), name="task"),
-    path('task/<int:pk>/', TaskDetail.as_view(), name="task")
+    path('task/<int:pk>/', TaskDetail.as_view(), name="task"),
+    path('create-task/', TaskCreate.as_view(), name='task-create')
 ]
--- UserProject/Check/views.py
+++ UserProject/Check/views.py
@@ -2,6 +2,8 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView
+from django.urls import reverse_lazy
 from .models import Task
 
 
@@ -18,5 +20,14 @@ class TaskDetail(DetailView):
     template_name='Check/task_detail.html'
     
 
+class TaskCreate(CreateView):
+    model = Task
+    fields = '__all__'
+    success_url= reverse_lazy('task')
+    
 # def pending(request):
 #     return render(request, 'Pending.html')
+
+class TaskUpdate(UpdateView):
+    model = Task
+    fields = '__all__'
